62a910ca487d9b2dc4fc561626c8f0c910cd85ce
Ekaterina Rodintseva <rodintseva@gmail.com>
2021-02-09 21:26:15 -0500

    First

---
+++ Spring-01-SpringContainer/src/main/java/CybertekApp.java
@@ -0,0 +1,11 @@
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CybertekApp {
+
+
+    public static void main(String[] args) {
+        BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
+
+    }
+}
