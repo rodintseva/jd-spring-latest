816e44147e158990ff7603941e6a3d48b4f24a6a
Ekaterina Rodintseva <rodintseva@gmail.com>
2021-02-10 21:40:29 -0500

    Singelton Prototype Bean Scope

---
+++ Spring-03-BeanScope-Singleton-Prototype/src/main/java/com/cybertek/CybertekApp.java
@@ -0,0 +1,19 @@
+package com.cybertek;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+
+        BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
+        Course course = container.getBean("javaHours",Course.class);
+        course.getTeachingHours();
+        Course course1 = container.getBean("seleniumHours",Course.class);
+        course1.getTeachingHours();
+
+
+    }
+}
+
