3d98b8870cc5f13a86b1c571ee64a5ef38ed8b3c
Ekaterina Rodintseva <rodintseva@gmail.com>
2021-02-10 19:47:04 -0500

    DI Constructor Injection

---
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,11 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+
+public class Java implements Course {
+    OfficeHours officeHours;
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours:" + (15 + officeHours.getHours()));
+    }
+}
